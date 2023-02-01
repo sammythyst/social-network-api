4af204cd1dc63a40140ae23a9dd3e2f1ab65219d
sasw <saswain96@gmail.com>
2023-02-01 12:15:05 -0600

    added user and thought models

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
-// app.use(require("./routes"));
+app.use(require("./routes"));
 
 mongoose.connect(process.env.NOMGODB_URI || "mongodb://localhost/social-network-api", {
     useFindAndModify: false,
