693ed66fd16ef44a7c61a8498d572075b96c64bf
sasw <saswain96@gmail.com>
2023-02-01 11:27:29 -0600

    initial commit

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+// app.use(require("./routes"));
+
+mongoose.connect(process.env.NOMGODB_URI || "mongodb://localhost/social-network-api", {
+    useFindAndModify: false,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+mongoose.set('debug', true);
+
+app.listen(PORT, () => console.log(`App listening at localhost:${PORT}`));
